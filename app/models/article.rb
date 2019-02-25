fac4b8162ba4ed289fe2a0e58667df97a00480bb
Nicholas Mills <nicholasrmills@icloud.com>
2019-02-25 16:25:13 +1100

    Using partials on view template to reduce redundant code

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
 
-    validates :title, presence: true, length: {minimum: 5, maximum: 20}
+    validates :title, presence: true, length: {minimum: 5}
     validates :text, presence: true
 end
