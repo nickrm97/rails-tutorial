54171e91574148d8208bbd095b2c0a7b04af9a7d
Nicholas Mills <nicholasrmills@icloud.com>
2019-02-25 15:47:54 +1100

    Adding an Update method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,37 @@
 class ArticlesController < ApplicationController
-    def new
-        puts "-------------------------------------"
-        puts "NEW (Method activated)"
-        puts "-------------------------------------"
-    end
-
     def index
         @articles = Article.all
     end
+    
+    def new
+        @article = Article.new
+    end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
     def create
         puts "-------------------------------------"
         puts "CREATE (Method activated)"
         puts "-------------------------------------"
         # byebug
         @article = Article.new(article_params)
-        @article.save
 
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
     end
 
     def show
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+
+    validates :title, presence: true, length: {minimum: 5, maximum: 20}
+    validates :text, presence: true
 end
