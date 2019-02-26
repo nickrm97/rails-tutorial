b39fbb13f9bc3b8649ee605d95efccc51cc766be
Nicholas Mills <nicholasrmills@icloud.com>
2019-02-26 12:47:42 +1100

    Added an articles resource

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,13 @@
+class ArticlesController < ApplicationController
+    def new 
+        # don't think this is possible until we have a model in
+        # @article =  Article.new
+    end
+
+    def create
+        # Need to do some sort of logic here to pass a form, create it if everything is g
+    end
+
+    def index
+    end
+end
