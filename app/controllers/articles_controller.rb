e9ffadcdfd84e906f60ac3921465f40c7ce11618
Nicholas Mills <nicholasrmills@icloud.com>
2019-02-27 13:35:31 +1100

    Working delete method, links and fun

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,41 @@
 class ArticlesController < ApplicationController
     def new 
         # don't think this is possible until we have a model in
-        # @article =  Article.new
+        # @article = Article.new
     end
 
     def create
-        # Need to do some sort of logic here to pass a form, create it if everything is g
+        @article = Article.new(article_params)
+
+        if @article.save 
+            puts "All good in the hood"
+            redirect_to @article
+        else 
+            puts "ERROR: Cannot save!"
+        end
     end
 
     def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+        # render plain: @article.title
+    end
+
+    def 
+
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
+    end
+    
+    private
+    def article_params
+        params.require(:article).permit(:title, :text)
     end
 end
